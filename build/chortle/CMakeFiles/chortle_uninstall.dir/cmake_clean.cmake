2af43375c160b57c86e77c0e19a9a9d4b725d21a
RS <rs@mage.me.uk>
2023-08-23 21:57:08 +0100

    ignore generated directories

--- build/chortle/CMakeFiles/chortle_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/chortle_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/chortle_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
